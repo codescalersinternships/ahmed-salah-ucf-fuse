b96f1c8961a6bcfb1b9df05a7d9f4bade1e62004
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2022-08-17 13:08:59 +0200

    Implement Interfaces of file
    - Implement Node interface by implementing Attr method
    - Implement HandleReadAller interface by implementing ReadAll method
    - Implement HandleReader interface by implementing Read method

--- file.go
+++ file.go
@@ -1,7 +1,37 @@
 package main
 
+import (
+	"log"
+
+	"bazil.org/fuse"
+	"bazil.org/fuse/fuseutil"
+	"golang.org/x/net/context"
+)
+
 
 type File struct {
 	Node
 	data []byte
+}
+
+// Attr provides the core information for the file
+func (f *File) Attr(ctx context.Context, a *fuse.Attr) error {
+	log.Println("Requested Attr for File", f.name, "has data size", len(f.data))
+	a.Inode = f.inode
+	a.Mode = 0777
+	a.Size = uint64(len(f.data))
+	return nil
+}
+
+// Read handles requests to read data from the file
+func (f *File) Read(ctx context.Context, req *fuse.ReadRequest, resp *fuse.ReadResponse) error {
+	log.Println("Requested Read on File", f.name)
+	fuseutil.HandleRead(req, resp, f.data)
+	return nil
+}
+
+// ReadAll reads all of the file
+func (f *File) ReadAll(ctx context.Context) ([]byte, error) {
+	log.Println("Reading all of file", f.name)
+	return []byte(f.data), nil
 }
